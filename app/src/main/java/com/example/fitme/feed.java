830ee5ff43b0d2d7b8bd39d27dd390efb4f63a6b
G-G-E <keunnalee@gmail.com>
2019-08-24 19:48:27 +0900

    8.24 수정중

--- app/src/main/java/com/example/fitme/feed.java
+++ app/src/main/java/com/example/fitme/feed.java
@@ -2,6 +2,7 @@ package com.example.fitme;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -14,6 +15,8 @@ import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class feed extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView; // 바텀 네이게이션 메뉴  -> 하단바
@@ -21,9 +24,15 @@ public class feed extends AppCompatActivity {
     ImageView imageView_recommendbot, imageView_notification;
 
 
+// 리사이클러뷰에 필요
+    ArrayList<feed_MainData> arrayList;
+    feed_Adapter feed_adapter;
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.e("feed","onCreate");
+        Log.e("feed", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
@@ -33,13 +42,14 @@ public class feed extends AppCompatActivity {
 
         /**여기서부터 리사이클러뷰 만들기**/
 
+        recyclerView = (RecyclerView)findViewById(R.id.feed_recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
+        arrayList = new ArrayList<>();
 
-
-
-
-
-
+        feed_adapter = new feed_Adapter(arrayList);
+        recyclerView.setAdapter(feed_adapter);
 
 
 
@@ -55,10 +65,10 @@ public class feed extends AppCompatActivity {
         });
 
 //하단바
-        bottomNavigationView = findViewById (R.id.bottomNavi);
+        bottomNavigationView = findViewById(R.id.bottomNavi);
         // 하단바 누를 때 색 바뀌게 하는 중
         Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem  = menu.getItem(0);
+        MenuItem menuItem = menu.getItem(0);
         menuItem.setChecked(true);
         //
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -66,25 +76,25 @@ public class feed extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
                 switch (menuItem.getItemId()) {
-                    case R.id.action_home :
-                        Intent home_intent = new Intent(feed.this,feed.class);
+                    case R.id.action_home:
+                        Intent home_intent = new Intent(feed.this, feed.class);
                         startActivity(home_intent);//액티비티 띄우기
 //                        startActivityForResult(intent,sub);//액티비티 띄우기
                         break;
-                    case R.id.action_search :
-                        Intent search_intent = new Intent(feed.this,searching.class);
+                    case R.id.action_search:
+                        Intent search_intent = new Intent(feed.this, searching.class);
                         startActivity(search_intent);//액티비티 띄우기
                         break;
-                    case R.id.action_write_review :
-                        Intent write_intent = new Intent(feed.this,review_category.class);
+                    case R.id.action_write_review:
+                        Intent write_intent = new Intent(feed.this, review_category.class);
                         startActivity(write_intent);//액티비티 띄우기
                         break;
-                    case R.id.action_insight :
-                        Intent insight_intent = new Intent(feed.this,insight.class);
+                    case R.id.action_insight:
+                        Intent insight_intent = new Intent(feed.this, insight.class);
                         startActivity(insight_intent);//액티비티 띄우기
                         break;
-                    case R.id.action_mycloset :
-                        Intent mycloset_intent = new Intent(feed.this,my_closet.class);
+                    case R.id.action_mycloset:
+                        Intent mycloset_intent = new Intent(feed.this, my_closet.class);
                         startActivity(mycloset_intent);//액티비티 띄우기
                         break;
                 }
@@ -94,7 +104,7 @@ public class feed extends AppCompatActivity {
 
             }
         });
-       //여기 내용까지 onCreate 안
+        //여기 내용까지 onCreate 안
     }
 
 
@@ -102,42 +112,42 @@ public class feed extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.e("feed","onRestart");
+        Log.e("feed", "onRestart");
         //액티비티가 중단되었다가 다시 시작
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e("feed","onStart");
+        Log.e("feed", "onStart");
         //액티비티가 화면에 나타나기 시작
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e("feed","onResume");
+        Log.e("feed", "onResume");
         //액티비티가 화면에 나타나고 상호작용이 가능해짐
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e("feed","onPause");
+        Log.e("feed", "onPause");
         //다른 액티비티가 시작되려함, 이 액티비티는 중단되려하고 백그라운드로 들어갑니다.
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e("feed","onStop");
+        Log.e("feed", "onStop");
         //액티비티가 더 이상 화면에 나타나지 않음,중단된 상태
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e("feed","onDestroy");
+        Log.e("feed", "onDestroy");
         //액티비티가 종료되려고 합니다.
     }
 
---
+++ app/src/main/java/com/example/fitme/feed_Adapter.java
@@ -0,0 +1,140 @@
+package com.example.fitme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+public class feed_Adapter extends RecyclerView.Adapter<feed_Adapter.FeedViewHolder>{
+
+    // 리사이클러뷰에 넣을 ArrayList
+
+    private ArrayList<feed_MainData> arrayList;
+
+    public feed_Adapter(ArrayList<feed_MainData>arrayList){   // 생성자
+        this.arrayList = arrayList ;
+    }
+
+    @NonNull
+    @Override
+    public feed_Adapter.FeedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_review_card,parent,false);
+        FeedViewHolder holder = new FeedViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    // 생성된 뷰홀더에 아이템 내용 세팅하는 곳
+    // 생성자로 현제 데이터가 들어오지 않기 때문에 position 값을 이용해서 데이터를 가지고 옴.
+
+    //TextView review_card,textView19,textView_mysize,textView_nickname, textView20, textView_shoppingmall_url, textView_likes_number, textView_likes,
+    // textView_detailed_review_card, textView_more, textView_hashtag1, textView_hashtag2, textView_hashtag3,textView_hashtag4;
+//ImageView imageView_reviewcard_profile_image, imageView_reviewcard_img1, imageView_reviewcard_img2, imageView_reviewcard_img3, imageView_reviewcard_img4, imageView_reviewcard_img5 ;
+//ImageButton imageButton_like,imageButton_comment, imageButton_bookmark ;
+    public void onBindViewHolder(@NonNull final feed_Adapter.FeedViewHolder holder, int position) {
+        holder.imageView_reviewcard_profile_image.setImageResource(arrayList.get(position).getImageView_reviewcard_profile_image());
+        holder.imageView_reviewcard_img1.setImageResource(arrayList.get(position).getImageView_reviewcard_img1());
+        holder.imageView_reviewcard_img2.setImageResource(arrayList.get(position).getImageView_reviewcard_img2());
+        holder.imageView_reviewcard_img3.setImageResource(arrayList.get(position).getImageView_reviewcard_img3());
+        holder.imageView_reviewcard_img4.setImageResource(arrayList.get(position).getImageView_reviewcard_img4());
+        holder.imageView_reviewcard_img5.setImageResource(arrayList.get(position).getImageView_reviewcard_img5());
+        holder.imageButton_like.setImageResource(arrayList.get(position).getImageButton_like());
+        holder.imageButton_comment.setImageResource(arrayList.get(position).getImageButton_comment());
+        holder.imageButton_bookmark.setImageResource(arrayList.get(position).getImageButton_bookmark());
+        holder.review_card.setText(arrayList.get(position).getReview_card());
+        holder.textView19.setText(arrayList.get(position).getTextView19());
+        holder.textView_mysize.setText(arrayList.get(position).getTextView_mysize());
+        holder.textView_nickname.setText(arrayList.get(position).getTextView_nickname());
+        holder.textView20.setText(arrayList.get(position).getTextView20());
+        holder.textView_shoppingmall_url.setText(arrayList.get(position).getTextView_shoppingmall_url());
+        holder.textView_likes_number.setText(arrayList.get(position).getTextView_likes_number());
+        holder.textView_likes.setText(arrayList.get(position).getTextView_likes());
+
+        holder.textView_detailed_review_card.setText(arrayList.get(position).getTextView_detailed_review_card());
+        holder.textView_more.setText(arrayList.get(position).getTextView_more());
+        holder.textView_hashtag1.setText(arrayList.get(position).getTextView_hashtag1());
+        holder.textView_hashtag2.setText(arrayList.get(position).getTextView_hashtag2());
+        holder.textView_hashtag3.setText(arrayList.get(position).getTextView_hashtag3());
+        holder.textView_hashtag4.setText(arrayList.get(position).getTextView_hashtag4());
+
+holder.itemView.findViewById(position);
+// holder.itemView.setTag(position);  // -> findViewById와 같은 기능
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             //클릭했을 때 어떤 걸 할건지
+            }
+        });
+
+        // 길게 눌렀을 때 어떤 걸 할건지
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                return true;
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();   // ArrayList의 사이즈만큼
+    }
+
+    public class FeedViewHolder extends RecyclerView.ViewHolder {
+
+        TextView review_card,textView19,textView_mysize,textView_nickname, textView20, textView_shoppingmall_url, textView_likes_number, textView_likes,
+         textView_detailed_review_card, textView_more, textView_hashtag1, textView_hashtag2, textView_hashtag3,textView_hashtag4;
+ImageView imageView_reviewcard_profile_image, imageView_reviewcard_img1, imageView_reviewcard_img2, imageView_reviewcard_img3, imageView_reviewcard_img4, imageView_reviewcard_img5 ;
+ImageButton imageButton_like,imageButton_comment, imageButton_bookmark ;
+
+        public FeedViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageView_reviewcard_profile_image = (ImageView) itemView.findViewById(R.id.imageView_reviewcard_profile_image);
+            this.imageView_reviewcard_img1 = (ImageView) itemView.findViewById(R.id.imageView_reviewcard_img1);
+            this.imageView_reviewcard_img2 = (ImageView) itemView.findViewById(R.id.imageView_reviewcard_img2);
+            this.imageView_reviewcard_img3 = (ImageView) itemView.findViewById(R.id.imageView_reviewcard_img3);
+            this.imageView_reviewcard_img4 = (ImageView) itemView.findViewById(R.id.imageView_reviewcard_img4);
+            this.imageView_reviewcard_img5 = (ImageView) itemView.findViewById(R.id.imageView_reviewcard_img5);
+            this.imageButton_like = (ImageButton) itemView.findViewById(R.id.imageButton_like);
+            this.imageButton_comment = (ImageButton) itemView.findViewById(R.id.imageButton_comment);
+            this.imageButton_bookmark = (ImageButton) itemView.findViewById(R.id.imageButton_bookmark);
+            this.review_card = (TextView) itemView.findViewById(R.id.review_card);
+            this.textView_mysize = (TextView) itemView.findViewById(R.id.textView_mysize);
+            this.textView_nickname = (TextView) itemView.findViewById(R.id.textView_nickname);
+            this.textView_shoppingmall_url = (TextView) itemView.findViewById(R.id.textView_shoppingmall_url);
+            this.textView_likes_number = (TextView) itemView.findViewById(R.id.textView_likes_number);
+            this.textView_likes = (TextView) itemView.findViewById(R.id.textView_likes);
+            this.textView_detailed_review_card = (TextView) itemView.findViewById(R.id.textView_detailed_review_card);
+            this.textView_more = (TextView) itemView.findViewById(R.id.textView_more);
+            this.textView_hashtag1 = (TextView) itemView.findViewById(R.id.textView_hashtag1);
+            this.textView_hashtag2 = (TextView) itemView.findViewById(R.id.textView_hashtag2);
+            this.textView19 = (TextView) itemView.findViewById(R.id.textView19);
+            this.textView20 = (TextView) itemView.findViewById(R.id.textView20);
+            this.textView_hashtag3 = (TextView) itemView.findViewById(R.id.textView_hashtag3);
+            this.textView_hashtag4 = (TextView) itemView.findViewById(R.id.textView_hashtag4);
+
+
+
+
+
+        }
+    }
+}
