1a8c500e5de17343abe3e77a19371c842646fa99
G-G-E <keunnalee@gmail.com>
2019-08-26 09:45:57 +0900

    8.26 ( 리사이클러뷰 추가,수정,삭제 ) + 댓글 창 작업중

---
+++ app/src/main/java/com/example/fitme/comment.java
@@ -0,0 +1,103 @@
+package com.example.fitme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class comment extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView; // 바텀 네이게이션 메뉴  -> 하단바
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e("review_written_by_me","onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_written_by_me);
+
+//하단바
+        bottomNavigationView = findViewById (R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home :
+                        Intent home_intent = new Intent(comment.this,feed.class);
+                        startActivity(home_intent);//액티비티 띄우기
+//                        startActivityForResult(intent,sub);//액티비티 띄우기
+                        break;
+                    case R.id.action_search :
+                        Intent search_intent = new Intent(comment.this,searching.class);
+                        startActivity(search_intent);//액티비티 띄우기
+                        break;
+                    case R.id.action_write_review :
+                        Intent write_intent = new Intent(comment.this,review_category.class);
+                        startActivity(write_intent);//액티비티 띄우기
+                        break;
+                    case R.id.action_insight :
+                        Intent insight_intent = new Intent(comment.this,insight.class);
+                        startActivity(insight_intent);//액티비티 띄우기
+                        break;
+                    case R.id.action_mycloset :
+                        Intent mycloset_intent = new Intent(comment.this,my_closet.class);
+                        startActivity(mycloset_intent);//액티비티 띄우기
+                        break;
+                }
+
+
+                return false;
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("review_written_by_me","onRestart");
+        //액티비티가 중단되었다가 다시 시작
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("review_written_by_me","onStart");
+        //액티비티가 화면에 나타나기 시작
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("review_written_by_me","onResume");
+        //액티비티가 화면에 나타나고 상호작용이 가능해짐
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("review_written_by_me","onPause");
+        //다른 액티비티가 시작되려함, 이 액티비티는 중단되려하고 백그라운드로 들어갑니다.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("review_written_by_me","onStop");
+        //액티비티가 더 이상 화면에 나타나지 않음,중단된 상태
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("review_written_by_me","onDestroy");
+        //액티비티가 종료되려고 합니다.
+    }
+
+}
